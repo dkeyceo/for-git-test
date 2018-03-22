c92742ac7cbc8f6ed77932ad1d9808c478deecad
dkeyceo <dkeyceo@gmail.com>
2018-03-22 09:39:53 +0200

    commit test

--- src/com/dkey/test/git/Main.java
+++ src/com/dkey/test/git/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello from test git!!!");
+        System.out.println("It is test commit!!!");
     }
 }
